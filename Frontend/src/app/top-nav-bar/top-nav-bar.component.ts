2a2cffcd6eb2f2847d7bf5e0f008e3d848f759ec
maciejows <owsianny.maciej@gmail.com>
2019-11-29 22:19:07 +0100

    Added styling for Top-Nav-Bar

--- Frontend/src/app/top-nav-bar/top-nav-bar.component.ts
+++ Frontend/src/app/top-nav-bar/top-nav-bar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-top-nav-bar',
   templateUrl: './top-nav-bar.component.html',
   styleUrls: ['./top-nav-bar.component.scss']
 })
-export class TopNavBarComponent implements OnInit {
+export class TopNavBarComponent{
+
+  options = ["Home", "About", "FAQ"];
+  selectedOption: string = "Home";
 
   constructor() { }
 
-  ngOnInit() {
+  selectOption(option: string){
+    this.selectedOption = option;
   }
+
 
 }
